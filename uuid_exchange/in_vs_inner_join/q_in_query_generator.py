e969bce9dfbe29284f5868bc3afd70e509af02ab
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-12-08 19:29:33 +0100

    Adding query generators and queries of various sizes

---
+++ uuid_exchange/in_vs_inner_join/q_in_query_generator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+print sys.argv
+
+command = list()
+command.append("\\timing on\n")
+command.append("SELECT db_dbnode_1.uuid AS db_dbnode_1_uuid ")
+command.append("FROM db_dbnode AS db_dbnode_1 ")
+command.append("WHERE db_dbnode_1.uuid IN (")
+
+limit = int(sys.argv[2])
+counter = 0
+with open(sys.argv[1]) as f:
+	for line in f:
+		if counter == limit:
+			break
+	
+		command.append('\'' + line.rstrip())
+		counter += 1
+	
+		# For the last comma
+		if counter == limit:
+			command.append('\'')
+		else:
+			command.append('\', ')
+    
+    
+command.append(");")
+print ''.join(command)
